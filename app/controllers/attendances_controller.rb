7da934e8817f31985c2b2fbfd2c01dec6adedada
heiann <akit26897@gmail.com>
2021-04-02 13:30:27 +0000

    ０４０２編集

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -30,7 +30,6 @@ class AttendancesController < ApplicationController
   end
 
   def update_one_month
-   
     ActiveRecord::Base.transaction do # トランザクションを開始します。
       attendances_params.each do |id, item|
         attendance = Attendance.find(id)
@@ -43,7 +42,8 @@ class AttendancesController < ApplicationController
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
-
+    
+  
   private
 
     # 1ヶ月分の勤怠情報を扱います。
